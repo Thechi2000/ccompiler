381fdc3253ddeb704d99061d7726c06f9b6fc664
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-12-26 18:43:31 +0100

    feat: implement tokeniser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+
+use clap::{command, Parser, Subcommand};
+use tokeniser::{IterReader, Tokeniser};
+
+mod tokeniser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Tokenise { file: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Tokenise { file } => {
+            let tok = Tokeniser::new(IterReader::new(File::open(file).unwrap()));
+
+            tok.for_each(|t| println!("{}", t));
+        }
+    }
 }
